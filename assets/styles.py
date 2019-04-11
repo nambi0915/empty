7eb008b0d6cc8f63d37a2b0ef258a995bf481e18
nambi <nambi.a@orzota.com>
2019-04-11 19:33:49 +0530

    added table and done UI changes

--- assets/styles.py
+++ assets/styles.py
@@ -1,6 +1,6 @@
 FRAME_PAGE_STYLE = """
 QFrame{
-    background-color: #FFFFFF;
+    background-color: white;
     border-radius: 0px;
     margin: 0px 0px 0px 0px;
     padding: 0px;
@@ -11,7 +11,7 @@ DIALOG_BUTTON = """
 QPushButton:enabled {
     font-family: SourceSansPro-Semibold;
     color: #FFFFFF;
-    background-color: #009CDE; 
+    background-color: orange; 
     border: none;
     border-radius: 1px;
     text-align: center;
@@ -30,5 +30,121 @@ QPushButton:disabled {
     text-decoration: none;
     font-size: 16px;
     padding: 5px 15px;
+}
+"""
+
+LABEL_HEADER = """
+QLabel {
+  font-family: SourceSansPro-bold;
+  font-size:19px;
+  color: #373737;
+  margin-bottom:1px;  
+}
+"""
+
+TABLE_WIDGET_STYLE = """
+QTableWidget
+{
+    alternate-background-color: #F5F5F5;
+    background-color: #FFFFFF;
+    border: none;
+    gridline-color: #FFFFFF;
+    font-family: SourceSansPro-Regular;
+    font-size: 14px;
+}
+
+QTableWidget::item
+{
+    padding: 0.3ex;
+    gridline-color: #FFFFFF;
+}
+
+QHeaderView
+{
+    font-family: SourceSansPro-Bold;
+    gridline-color: #FFFFFF;
+    vertical-alignment
+}
+
+QHeaderView::section 
+{
+    font-size: 16px;
+    border: none;
+    margin-bottom:0px;
+    background-color: white;    
+}
+
+QTableWidget::item:pressed
+{
+    background: #33A4DF;
+    color: #FFFFFF;
+}
+
+QTableWidget::item:selected:active
+{
+    background: #33A4DF;
+    color: #FFFFFF;
+}
+
+QTableWidget::item:selected:hover
+{
+    background-color: #009CDE;
+    color: #FFFFFF;
+}
+
+QTableWidget::item:hover
+{
+    background-color:#009CDE; 
+    color:#FFFFFF;
+}
+"""
+
+SCROLL_AREA = """
+QScrollArea {
+   background: #FFFFFF;
+}
+
+QScrollBar:vertical {
+   background-color: #F5F5F5;
+   margin: 20px 0 20px 0;
+}
+
+QScrollBar::handle:vertical {
+   border-radius: 3px;
+   border: 2px solid #CBCBCB;
+   background: #F1F1F1;
+}
+
+QScrollBar::add-line:vertical {
+   background: #FFFFFF;
+   subcontrol-origin: margin;
+   subcontrol-position: bottom;
+   height: 20px;
+}
+
+QScrollBar::sub-line:vertical {
+   background: #FFFFFF;
+   subcontrol-origin: margin;
+   subcontrol-position: top;
+   height: 20px;
+}
+
+
+QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
+   background: #FAFAFA;
+   border: 2px solid #CBCBCB;
+   border-radius: 3px;
+}
+
+QScrollBar::up-arrow:vertical {
+   image: url(assets/style/up_arrow.png);
+   width: 20px;
+   height: 20px;
+}
+
+QScrollBar::down-arrow:vertical {
+   image: url(assets/style/down_arrow.png);
+   width: 20px;
+   height: 20px;
 }
 """
--- ui/MainUI.py
+++ ui/MainUI.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QSizePolicy, QVBoxLayout, QPushButton, QSplitter, QHBoxLayout, QFrame
+from PyQt5.QtWidgets import (QWidget, QSizePolicy, QVBoxLayout, QPushButton, QSplitter, QHBoxLayout, QFrame, QLabel,
+                             QTableWidget, QAbstractItemView, QAbstractScrollArea, QHeaderView)
 from assets import styles
 from PyQt5.QtCore import Qt
 
@@ -26,23 +27,91 @@ class MainUI(QWidget):
         layout.addWidget(splitter)
         self.setLayout(layout)
 
-    def _initialize_left_frame(self):
+    def new_qframe(self):
         frame = QFrame()
         frame.setStyleSheet(styles.FRAME_PAGE_STYLE)
+        return frame
+
+    def _initialize_left_frame(self):
+        frame = self.new_qframe()
         frame.setMaximumWidth(200)
+        splitter = QSplitter(Qt.Vertical)
+        right_top_frame = self._initialize_right_top_frame()
+        right_bottom_frame = self._initialize_right_bottom_frame()
+        splitter.addWidget(right_top_frame)
+        splitter.addWidget(right_bottom_frame)
         layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignTop)
-        select_folder_button=QPushButton("Select Folder")
+
+        layout.addWidget(splitter)
+        frame.setLayout(layout)
+        return frame
+
+    def _initialize_right_frame(self):
+        frame = self.new_qframe()
+        layout = QVBoxLayout()
+        splitter = QSplitter(Qt.Vertical)
+        left_top_frame = self._initialize_left_top_frame()
+        left_bottom_frame = self._initialize_left_bottom_frame()
+        splitter.addWidget(left_top_frame)
+        splitter.addWidget(left_bottom_frame)
+        layout.addWidget(splitter)
+        frame.setLayout(layout)
+        return frame
+
+    def _initialize_right_top_frame(self):
+        frame = self.new_qframe()
+        frame.setMaximumHeight(50)
+        layout = QHBoxLayout()
+        layout.setAlignment(Qt.AlignCenter)
+        select_folder_button = QPushButton("Select Folder")
         select_folder_button.setStyleSheet(styles.DIALOG_BUTTON)
         select_folder_button.setMaximumWidth(160)
+        select_folder_button.clicked.connect(self.select_button_clicked)
 
         layout.addWidget(select_folder_button)
         frame.setLayout(layout)
         return frame
 
-    def _initialize_right_frame(self):
-        frame = QFrame()
-        frame.setStyleSheet(styles.FRAME_PAGE_STYLE)
-        layout = QVBoxLayout()
+    def _initialize_right_bottom_frame(self):
+        frame = self.new_qframe()
+        layout = QHBoxLayout()
         frame.setLayout(layout)
         return frame
+
+    def _initialize_left_top_frame(self):
+        frame = self.new_qframe()
+        frame.setMaximumHeight(50)
+        layout = QHBoxLayout()
+        layout.setAlignment(Qt.AlignCenter)
+        header = QLabel('Empty Directories')
+        header.setStyleSheet(styles.LABEL_HEADER)
+        layout.addWidget(header)
+        frame.setLayout(layout)
+        return frame
+
+    def _initialize_left_bottom_frame(self):
+        frame = self.new_qframe()
+        layout = QHBoxLayout()
+        self._initialize_folder_table_view()
+
+        layout.addWidget(self.folder_list_view)
+        frame.setLayout(layout)
+        return frame
+
+    def select_button_clicked(self):
+        print('Folder')
+
+    def _initialize_folder_table_view(self):
+        self.folder_list_view = self._get_table_widget()
+        self.folder_list_view.setColumnCount(4)
+        self.folder_list_view.setHorizontalHeaderLabels(['Folder Name', 'Path', 'Created time', 'Deleted time'])
+        self.folder_list_view.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.folder_list_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+    def _get_table_widget(self):
+        table_widget = QTableWidget()
+        table_widget.setStyleSheet(styles.TABLE_WIDGET_STYLE)
+        table_widget.verticalHeader().setVisible(False)
+        table_widget.horizontalHeader().setDefaultAlignment(Qt.AlignCenter)
+        table_widget.verticalScrollBar().setStyleSheet(styles.SCROLL_AREA)
+        return table_widget
